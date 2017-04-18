01fb56f2af69540838519d75b0cc9260e28f938e
Havard Haug <haavardhaug@gmail.com>
2017-04-18 23:28:41 +0200

    Initial creation

---
+++ desktop/src/com/doncapo/game/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.doncapo.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.doncapo.game.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Main";
+		config.width = 800;
+		config.height = 480;
+		new LwjglApplication(new Main(), config);
+	}
+}
---
+++ html/src/com/doncapo/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.doncapo.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.doncapo.game.Main;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(800, 480);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Main();
+        }
+}
